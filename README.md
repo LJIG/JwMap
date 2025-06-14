0b374e2cb7e235bcb0c9b6c51ced07b490b46e26
LJIG <389701057@qq.com>
2025-06-14 11:15:25 +0800

    + init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JwMap
+
+一个基于Vue 3和高德地图的轻量级地图组件库。
+
+## 安装
+
+```bash
+npm install jwmap
+```
+
+## 使用方法
+
+```js
+import JwMap from 'jwmap'
+
+// 使用示例
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Author             Bian <389701057@qq.com>
+ * Date               2025-06-14 10:30:31
+ * LastEditors        LJIG <389701057@qq.com>
+ * LastEditTime       2025-06-14 10:30:32
+ * FilePath           /index.js
+ * Description        
+ * Copyright (c) 2025 by Bian <389701057@qq.com>, All Rights Reserved. 
+ */
+
+console.log('hello world');
